d5e5fe5756ece46fe25a83432d7b9ae185a411d0
Mesfin Mebrate <mesi91@hotmail.com>
2016-07-15 17:38:05 +0100

    Turned LifeManager into a Play 2.4.8 application that uses Neo4j.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,9 +1,48 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import org.anormcypher.{Cypher, Neo4jREST}
+import play.api._
+import play.api.libs.ws.ning
+import play.api.mvc._
 
-object Application extends Controller {
+class Application extends Controller {
+
   def index = Action {
-    Ok(views.html.index("Hello Play Framework"))
+    Ok(views.html.index("Your new application is ready."))
   }
+
+  def testNeo = Action {
+
+    // Provide an instance of WSClient
+    val wsclient = ning.NingWSClient()
+
+    // Setup the Rest Client
+    // Need to add the Neo4jConnection type annotation so that the default
+    // Neo4jConnection -> Neo4jTransaction conversion is in the implicit scope
+    implicit val connection = Neo4jREST("localhost", 7474, "neo4j", "neoistheone")(wsclient)
+
+    // Provide an ExecutionContext
+    implicit val ec = scala.concurrent.ExecutionContext.global
+
+    // create some test nodes
+    Cypher("""create (anorm:anormcyphertest {name:"AnormCypher"}), (test:anormcyphertest {name:"Test"})""").execute()
+
+    // a simple query
+    val req = Cypher("match (n:anormcyphertest) return n.name")
+
+    // get a stream of results back
+    val stream = req()
+
+    // get the results and put them into a list
+    stream.map(row => {row[String]("n.name")}).toList
+
+    //    // remove the test nodes
+    //    Cypher("match (n:anormcyphertest) delete n")()
+
+    // shut down WSClient
+    wsclient.close()
+
+    Ok("It works!\n")
+  }
+
 }
--- build.sbt
+++ build.sbt
@@ -1,12 +1,30 @@
-import play.Project._
-
-name := "hello-play-scala"
+name := "play-scala"
 
 version := "1.0-SNAPSHOT"
 
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.8"
+
 libraryDependencies ++= Seq(
-  "org.webjars" %% "webjars-play" % "2.2.2", 
-  "org.webjars" % "bootstrap" % "2.3.1"
+  jdbc,
+  cache,
+  ws,
+  specs2 % Test
 )
 
-playScalaSettings
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+
+resolvers ++= Seq(
+  "anormcypher" at "http://repo.anormcypher.org/",
+  "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
+)
+
+
+libraryDependencies ++= Seq(
+  "org.anormcypher" %% "anormcypher" % "0.9.1"
+)
+
+// Play provides two styles of routers, one expects its actions to be injected, the
+// other, legacy style, accesses its actions statically.
+routesGenerator := InjectedRoutesGenerator
