7e79079d87df1fb05ef63e3353713a0d1f79d297
Mesfin Mebrate <mesi91@hotmail.com>
2016-08-20 19:53:42 +0000

    Edited neo4j/package.

--- app/neo4j/package.scala
+++ app/neo4j/package.scala
@@ -71,6 +71,7 @@ package object neo4j {
       resultRow match {
         case CypherRow(row: Map[String, Any]) => Hobby(
           id = Id(row.get("id").get.asInstanceOf[String]),
+          goalId = Id(row.get("id").get.asInstanceOf[String]),
           summary = row.get("summary").get.asInstanceOf[String],
           description = row.get("description").get.asInstanceOf[String],
           typeOf = HobbyType.withName(row.get("typeOf").get.asInstanceOf[String]),
@@ -94,16 +95,54 @@ package object neo4j {
       }
     }
 
-    /*LaserDonut (
-       id: Id,
-       summary: String,
-       description: String,
-       goalId: Id,
-       status: StatusType.Value,
-       milestone: String,
-       order: Int,
-       typeOf: DonutType.Value
-    )*/
+    def asPortion: Portion = {
+      resultRow match {
+        case CypherRow(row: Map[String, Any]) => Portion(
+          id = Id(row.get("id").get.asInstanceOf[String]),
+          laserDonutId = Id(row.get("laserDonutId").get.asInstanceOf[String]),
+          summary = row.get("summary").get.asInstanceOf[String],
+          order = row.get("order").asInstanceOf[Int],
+          status = StatusType.withName(row.get("status").get.asInstanceOf[String])
+        )
+      }
+    }
+
+    def asReceipt: Receipt = {
+      resultRow match {
+        case CypherRow(row: Map[String, Any]) => Receipt(
+          purchasedItem = row.get("purchasedItem").get.asInstanceOf[String],
+          expenditure = row.get("expenditure").get.asInstanceOf[Double],
+          nameOfEstablishment = row.get("nameOfEstablishment").get.asInstanceOf[String]
+        )
+      }
+    }
+
+    def asSaturday: Saturday = {
+      resultRow match {
+        case CypherRow(row: Map[String, Any]) => Saturday(
+          id = Id(row.get("id").get.asInstanceOf[String]),
+          weekId = Id(row.get("weekId").get.asInstanceOf[String]),
+          date = row.get("date").get.asInstanceOf[Long],
+          threads = row.get("threads").asInstanceOf[Seq[String]].map(id => Id(id)),
+          portion = row.get("portion").map(item => Id(item.asInstanceOf[String])),
+          passiveHobby = row.get("passiveHobby").map(item => Id(item.asInstanceOf[String])),
+          financialTracking = row.get("financialTracking").map(item => Id(item.asInstanceOf[String]))
+        )
+      }
+    }
+
+    def asSunday: Sunday = {
+      resultRow match {
+        case CypherRow(row: Map[String, Any]) => Sunday(
+          id = Id(row.get("id").get.asInstanceOf[String]),
+          weekId = Id(row.get("weekId").get.asInstanceOf[String]),
+          date = row.get("date").get.asInstanceOf[Long],
+          threads = row.get("threads").asInstanceOf[Seq[String]].map(id => Id(id)),
+          activeHobby = row.get("activeHobby").map(item => Id(item.asInstanceOf[String])),
+          financialTracking = row.get("financialTracking").map(item => Id(item.asInstanceOf[String]))
+        )
+      }
+    }
 
     def asTheme: Theme = {
       resultRow match {
@@ -111,6 +150,79 @@ package object neo4j {
           id = Id(row.get("id").get.asInstanceOf[String]),
           yearId = Id(row.get("yearId").get.asInstanceOf[String]),
           name = row.get("name").get.asInstanceOf[String]
+        )
+      }
+    }
+
+    def asThread: Thread = {
+      resultRow match {
+        case CypherRow(row: Map[String, Any]) => Thread(
+          id = Id(row.get("id").get.asInstanceOf[String]),
+          summary = row.get("summary").get.asInstanceOf[String],
+          description = row.get("description").get.asInstanceOf[String],
+          goalId = Id(row.get("goalId").get.asInstanceOf[String]),
+          status = StatusType.withName(row.get("status").get.asInstanceOf[String])
+        )
+      }
+    }
+
+    def asToDo: ToDo = {
+      resultRow match {
+        case CypherRow(row: Map[String, Any]) => ToDo(
+          id = Id(row.get("id").get.asInstanceOf[String]),
+          portionId = Id(row.get("portionId").get.asInstanceOf[String]),
+          description = row.get("id").get.asInstanceOf[String],
+          order = row.get("order").get.asInstanceOf[Int],
+          status = StatusType.withName(row.get("status").get.asInstanceOf[String])
+        )
+      }
+    }
+
+    def asWeave: Weave = {
+      resultRow match {
+        case CypherRow(row: Map[String, Any]) => Weave(
+          id = Id(row.get("id").get.asInstanceOf[String]),
+          summary = row.get("summary").get.asInstanceOf[String],
+          description = row.get("description").get.asInstanceOf[String],
+          goalId = Id(row.get("goalId").get.asInstanceOf[String]),
+          status = StatusType.withName(row.get("status").get.asInstanceOf[String]),
+          typeOf = WeaveType.withName(row.get("typeOf").get.asInstanceOf[String])
+        )
+      }
+    }
+
+    def asWeek: Week = {
+      resultRow match {
+        case CypherRow(row: Map[String, Any]) => Week(
+          id = Id(row.get("id").get.asInstanceOf[String]),
+          startDate = row.get("startDate").get.asInstanceOf[Long],
+          finishDate = row.get("finishDate").get.asInstanceOf[Long],
+          weave = row.get("weave").map(item => Id(item.asInstanceOf[String])),
+          laserDonut = row.get("laserDonut").map(item => Id(item.asInstanceOf[String]))
+        )
+      }
+    }
+
+    def asWeekDay: WeekDay = {
+      resultRow match {
+        case CypherRow(row: Map[String, Any]) => WeekDay(
+          id = Id(row.get("id").get.asInstanceOf[String]),
+          weekId = Id(row.get("weekId").get.asInstanceOf[String]),
+          date = row.get("date").get.asInstanceOf[Long],
+          threads = row.get("threads").asInstanceOf[Seq[String]].map(id => Id(id)),
+          weave = row.get("weave").map(item => Id(item.asInstanceOf[String])),
+          portion = row.get("portion").map(item => Id(item.asInstanceOf[String])),
+          financialTracking = row.get("financialTracking").map(item => Id(item.asInstanceOf[String]))
+        )
+      }
+    }
+
+    def asYear: Year = {
+      resultRow match {
+        case CypherRow(row: Map[String, Any]) => Year(
+          id = Id(row.get("id").get.asInstanceOf[String]),
+          startDate = row.get("startDate").get.asInstanceOf[Long],
+          finishDate = row.get("finishDate").get.asInstanceOf[Long]
         )
       }
     }
