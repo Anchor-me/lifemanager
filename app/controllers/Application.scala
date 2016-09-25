07d14e42853f69b560339d426fe9536348fe6f76
Mesfin Mebrate <mesi91@hotmail.com>
2016-09-25 22:56:57 +0100

    Moved unnecessary code.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,12 +15,9 @@ class Application extends Controller {
   }
 
   def add(spokeName: String) = Action { request =>
-    import GenericJsonConversions._
     val item = request.body.asJson.get
     SpokeType.withName(spokeName) match {
       case SpokeType.BacklogItem => DatabaseLayer.add(item.as[BacklogItem])
-      case SpokeType.BufferBlock => DatabaseLayer.add(item.as[BufferBlock])
-      case SpokeType.ConcreteBlock => DatabaseLayer.add(item.as[ConcreteBlock])
       case SpokeType.Epoch => DatabaseLayer.add(item.as[Epoch])
       case SpokeType.FinancialTracking => DatabaseLayer.add(item.as[FinancialTracking])
       case SpokeType.Goal => DatabaseLayer.add(item.as[Goal])
@@ -28,14 +25,10 @@ class Application extends Controller {
       case SpokeType.LaserDonut => DatabaseLayer.add(item.as[LaserDonut])
       case SpokeType.Portion => DatabaseLayer.add(item.as[Portion])
       case SpokeType.Receipt => DatabaseLayer.add(item.as[Receipt])
-      case SpokeType.Saturday => DatabaseLayer.add(item.as[Saturday])
-      case SpokeType.Sunday => DatabaseLayer.add(item.as[Sunday])
       case SpokeType.Theme => DatabaseLayer.add(item.as[Theme])
       case SpokeType.Thread => DatabaseLayer.add(item.as[Thread])
       case SpokeType.ToDo => DatabaseLayer.add(item.as[ToDo])
       case SpokeType.Weave => DatabaseLayer.add(item.as[Weave])
-      case SpokeType.Week => DatabaseLayer.add(item.as[Week])
-      case SpokeType.WeekDay => DatabaseLayer.add(item.as[WeekDay])
       case SpokeType.Year => DatabaseLayer.add(item.as[Year])
     }
     Ok
@@ -53,8 +46,6 @@ class Application extends Controller {
         val answer: JsValue = {
           SpokeType.withName(spokeName) match {
             case SpokeType.BacklogItem => Json.toJson(item.asBacklogItem)
-            case SpokeType.BufferBlock => Json.toJson(item.asBufferBlock)
-            case SpokeType.ConcreteBlock => Json.toJson(item.asConcreteBlock)
             case SpokeType.Epoch => Json.toJson(item.asEpoch)
             case SpokeType.FinancialTracking => Json.toJson(item.asFinancialTracking)
             case SpokeType.Goal => Json.toJson(item.asGoal)
@@ -62,15 +53,11 @@ class Application extends Controller {
             case SpokeType.LaserDonut => Json.toJson(item.asLaserDonut)
             case SpokeType.Portion => Json.toJson(item.asPortion)
             case SpokeType.Receipt => Json.toJson(item.asReceipt)
-            case SpokeType.Saturday => Json.toJson(item.asSaturday)
-            case SpokeType.Sunday => Json.toJson(item.asSunday)
             case SpokeType.Theme => Json.toJson(item.asTheme)
             case SpokeType.Thread => Json.toJson(item.asThread)
             case SpokeType.Timetable => Json.toJson(item.asTimetable)
             case SpokeType.ToDo => Json.toJson(item.asToDo)
             case SpokeType.Weave => Json.toJson(item.asWeave)
-            case SpokeType.Week => Json.toJson(item.asWeek)
-            case SpokeType.WeekDay => Json.toJson(item.asWeekDay)
             case SpokeType.Year => Json.toJson(item.asYear)
           }
         }
--- app/db/DatabaseLayer.scala
+++ app/db/DatabaseLayer.scala
@@ -13,17 +13,6 @@ object DatabaseLayer {
     NeoService.connect(backlogItem.id.id, "is recorded during", backlogItem.yearId.id)
   }
 
-  def add(bufferBlock: BufferBlock): Unit = {
-    NeoService.add(bufferBlock)
-    bufferBlock.firstTask.foreach(task => NeoService.connect(bufferBlock.id.id, "firstly runs", task.id))
-    bufferBlock.secondTask.foreach(task => NeoService.connect(bufferBlock.id.id, "secondly runs", task.id))
-  }
-
-  def add(concreteBlock: ConcreteBlock): Unit = {
-    NeoService.add(concreteBlock)
-    concreteBlock.task.foreach(task => NeoService.connect(concreteBlock.id.id, "only runs", task.id))
-  }
-
   def add(epoch: Epoch): Unit = {
     NeoService.add(epoch)
   }
@@ -59,21 +48,6 @@ object DatabaseLayer {
     NeoService.connect(receipt.id.id, "is tracked in", receipt.trackingId.id)
   }
 
-  def add(saturday: Saturday): Unit = {
-    NeoService.add(saturday)
-    NeoService.connect(saturday.id.id, "occurs during", saturday.weekId.id)
-    saturday.threads.foreach(thread => NeoService.connect(saturday.id.id, "contains", thread.id))
-    saturday.portion.foreach(portion => NeoService.connect(saturday.id.id, "contains", portion.id))
-    saturday.passiveHobby.foreach(hobby => NeoService.connect(saturday.id.id, "contains", hobby.id))
-  }
-
-  def add(sunday: Sunday): Unit = {
-    NeoService.add(sunday)
-    NeoService.connect(sunday.id.id, "occurs in", sunday.weekId.id)
-    sunday.threads.foreach(thread => NeoService.connect(sunday.id.id, "contains", thread.id))
-    sunday.activeHobby.foreach(hobby => NeoService.connect(sunday.id.id, "contains", hobby.id))
-  }
-
   def add(theme: Theme): Unit = {
     NeoService.add(theme)
     NeoService.connect(theme.id.id, "from year", theme.yearId.id)
@@ -82,12 +56,6 @@ object DatabaseLayer {
   def add(thread: Thread): Unit = {
     NeoService.add(thread)
     thread.goalId.foreach(goal => NeoService.connect(thread.id.id, "originates from", goal.id))
-  }
-
-  def add(timetable: Timetable): Unit = {
-    NeoService.add(timetable)
-    NeoService.connect(timetable.id.id, "is used during", timetable.dayId.id)
-    timetable.scheduledItems.foreach(item => NeoService.connect(timetable.id.id, "runs", item.id))
   }
 
   def add(toDo: ToDo): Unit = {
@@ -100,25 +68,8 @@ object DatabaseLayer {
     weave.goalId.foreach(goal => NeoService.connect(weave.id.id, "originates from", goal.id))
   }
 
-  def add(week: Week): Unit = {
-    NeoService.add(week)
-    NeoService.connect(week.id.id, "occurs in", week.yearId.id)
-    week.threads.foreach(thread => NeoService.connect(week.id.id, "contains", thread.id))
-    week.weaves.foreach(weave => NeoService.connect(week.id.id, "contains", weave.id))
-    week.laserDonut.foreach(laserDonut => NeoService.connect(week.id.id, "contains", laserDonut.id))
-  }
-
-  def add(weekDay: WeekDay): Unit = {
-    NeoService.add(weekDay)
-    NeoService.connect(weekDay.id.id, "occurs during", weekDay.weekId.id)
-    weekDay.threads.foreach(thread => NeoService.connect(weekDay.id.id, "contains", thread.id))
-    weekDay.weaves.foreach(weave => NeoService.connect(weekDay.id.id, "contains", weave.id))
-    weekDay.portion.foreach(portion => NeoService.connect(weekDay.id.id, "contains", portion.id))
-  }
-
   def add(year: Year): Unit = {
     NeoService.add(year)
     NeoService.connect(year.id.id, "occurs during", year.epochId.id)
-    year.threads.foreach(thread => NeoService.connect(year.id.id, "contains", thread.id))
   }
 }
