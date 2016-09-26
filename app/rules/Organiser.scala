ea53096ab0c33c6e1b419715430810bce8cd29f9
Mesfin Mebrate <mesi91@hotmail.com>
2016-09-26 22:43:11 +0100

    Started writing routines.

--- app/rules/Organiser.scala
+++ app/rules/Organiser.scala
@@ -1,8 +1,13 @@
 package rules
 
+import com.anchor.model.Timetable
+
 /**
   * Created by mesfinmebrate on 25/09/2016.
   */
 object Organiser {
 
+  def getCurrentTimetable: Timetable = {
+
+  }
 }
