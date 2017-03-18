fd94ab7b8346a7eeea1920b02898a5be83af6d56
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2017-03-18 21:39:21 +0000

    Made tiny configuration changes.

--- app/db/neo4j/NeoConfiguration.scala
+++ app/db/neo4j/NeoConfiguration.scala
@@ -12,10 +12,10 @@ object NeoConfiguration {
 
   def apply(wsclient: WSClient): Neo4jREST = {
     Neo4jREST(
-      host = Play.application.configuration.getString("db.neo4j.host").get,
-      port = Play.application.configuration.getString("db.neo4j.port").get.toInt,
-      username = Play.application.configuration.getString("db.neo4j.username").get,
-      password = Play.application.configuration.getString("db.neo4j.password").get
+      host = Play.application.configuration.getString("neo4j.host").get,
+      port = Play.application.configuration.getString("neo4j.port").get.toInt,
+      username = Play.application.configuration.getString("neo4j.username").get,
+      password = Play.application.configuration.getString("neo4j.password").get
     )(wsclient)
   }
 }
